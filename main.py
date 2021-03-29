7e464ebbca1d5d2d5fb4518eca22b2bb98bdac69
Marrit <marritschellekens@yahoo.com>
2021-03-29 10:05:26 +0200

    Added yolo for video file

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ Created on Sat Mar 27 12:09:08 2021
 @author: marri
 """
 
+
 import yolo_opencv as yolo
+import yolo_video as yolo_video
+import imutils
+from imutils.video import FPS
+from imutils.video import VideoStream
 
 yolo.find_vehicles("test4.png")
+yolo_video.find_vehicles("test_short.mp4", "Output_short.mp4")
